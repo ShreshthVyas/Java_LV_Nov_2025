1c4f08934b485f2ad004b413984e986894f52bf4
SHRESHTH VYAS <shreshth.vyas.2000@gmail.com>
2025-12-26 23:03:22 +0530

    String QPS

---
+++ Crx_November_Live_2025/src/Lecture16/StringtoAny.java
@@ -0,0 +1,15 @@
+package Lecture16;
+
+public class StringtoAny {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "123";
+		int a = Integer.parseInt(s);
+		long l = Long.parseLong(s);
+		String ss = a+"";
+		System.out.println(l+2);
+		System.out.println(a+2);
+	}
+
+}
